d8d5b33efe320ea2bfb4626f2855a5851f45c63a
jparmar32 <jsparmar@stanford.edu>
2021-11-12 00:13:15 -0800

    acgan changes

--- acgan/generator.py
+++ acgan/generator.py
@@ -3,32 +3,64 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
-class Generator(nn.Module):
-    def __init__(self):
-        super(Generator, self).__init__()
 
-        self.label_emb = nn.Embedding(2, 100)
+class _netG(nn.Module):
+    def __init__(self, ngpu, nz):
+        super(_netG, self).__init__()
+        self.ngpu = ngpu
+        self.nz = nz
 
-        self.init_size = 64 // 4  # Initial size before upsampling
-        self.l1 = nn.Sequential(nn.Linear(100, 128 * self.init_size ** 2))
-
-        self.conv_blocks = nn.Sequential(
-            nn.BatchNorm2d(128),
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(128, 128, 3, stride=1, padding=1),
-            nn.BatchNorm2d(128, 0.8),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(128, 64, 3, stride=1, padding=1),
-            nn.BatchNorm2d(64, 0.8),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(64, 1, 3, stride=1, padding=1),
+        # first linear layer
+        self.fc1 = nn.Linear(110, 768)
+        # Transposed Convolution 2
+        self.tconv2 = nn.Sequential(
+            nn.ConvTranspose2d(768, 384, 5, 2, 0, bias=False),
+            nn.BatchNorm2d(384),
+            nn.ReLU(True),
+        )
+        # Transposed Convolution 3
+        self.tconv3 = nn.Sequential(
+            nn.ConvTranspose2d(384, 256, 5, 2, 0, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+        )
+        # Transposed Convolution 4
+        self.tconv4 = nn.Sequential(
+            nn.ConvTranspose2d(256, 192, 5, 2, 0, bias=False),
+            nn.BatchNorm2d(192),
+            nn.ReLU(True),
+        )
+        # Transposed Convolution 5
+        self.tconv5 = nn.Sequential(
+            nn.ConvTranspose2d(192, 64, 5, 2, 0, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+        )
+        # Transposed Convolution 5
+        self.tconv6 = nn.Sequential(
+            nn.ConvTranspose2d(64, 3, 8, 2, 0, bias=False),
             nn.Tanh(),
         )
 
-    def forward(self, noise, labels):
-        gen_input = torch.mul(self.label_emb(labels), noise)
-        out = self.l1(gen_input)
-        out = out.view(out.shape[0], 128, self.init_size, self.init_size)
-        img = self.conv_blocks(out)
-        return img
+    def forward(self, input):
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            input = input.view(-1, self.nz)
+            fc1 = nn.parallel.data_parallel(self.fc1, input, range(self.ngpu))
+            fc1 = fc1.view(-1, 768, 1, 1)
+            tconv2 = nn.parallel.data_parallel(self.tconv2, fc1, range(self.ngpu))
+            tconv3 = nn.parallel.data_parallel(self.tconv3, tconv2, range(self.ngpu))
+            tconv4 = nn.parallel.data_parallel(self.tconv4, tconv3, range(self.ngpu))
+            tconv5 = nn.parallel.data_parallel(self.tconv5, tconv4, range(self.ngpu))
+            tconv5 = nn.parallel.data_parallel(self.tconv6, tconv5, range(self.ngpu))
+            output = tconv5
+        else:
+            input = input.view(-1, self.nz)
+            fc1 = self.fc1(input)
+            fc1 = fc1.view(-1, 768, 1, 1)
+            tconv2 = self.tconv2(fc1)
+            tconv3 = self.tconv3(tconv2)
+            tconv4 = self.tconv4(tconv3)
+            tconv5 = self.tconv5(tconv4)
+            tconv5 = self.tconv6(tconv5)
+            output = tconv5
+        return output
--- train_acgan.py
+++ train_acgan.py
@@ -16,8 +16,8 @@ import torch
 
 from tqdm import tqdm 
 from dataloader import fetch_entire_dataloader
-from acgan.generator import Generator
-from acgan.discriminator import Discriminator
+from acgan.generator import _netG
+from acgan.discriminator import _netD
 
 cuda = True if torch.cuda.is_available() else False
 
@@ -30,120 +30,167 @@ def weights_init_normal(m):
         torch.nn.init.constant_(m.bias.data, 0.0)
 
 
-# Loss functions
-adversarial_loss = torch.nn.BCELoss()
-auxiliary_loss = torch.nn.CrossEntropyLoss()
+def compute_acc(preds, labels):
+    correct = 0
+    preds_ = preds.data.max(1)[1]
+    correct = preds_.eq(labels.data).cpu().sum()
+    acc = float(correct) / float(len(labels.data)) * 100.0
+    return acc
 
-# Initialize generator and discriminator
-generator = Generator()
-discriminator = Discriminator()
-
-if cuda:
-    generator.cuda()
-    discriminator.cuda()
-    adversarial_loss.cuda()
-    auxiliary_loss.cuda()
-
-# Initialize weights
-generator.apply(weights_init_normal)
-discriminator.apply(weights_init_normal)
 
 # Configure data loader
 dl = fetch_entire_dataloader("cxr_p","/media",0.2,2,32,4, gaze_task="cam_reg_convex", gan = True)
 
-# Optimizers
-optimizer_G = torch.optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
-optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
-
-FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
-LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
 
 
-def sample_image(n_row, batches_done):
-    """Saves a grid of generated digits ranging from 0 to n_classes"""
-    # Sample noise
-    z = Variable(FloatTensor(np.random.normal(0, 1, (n_row ** 2, opt.latent_dim))))
-    # Get labels ranging from 0 to n_classes for n rows
-    labels = np.array([num for _ in range(n_row) for num in range(n_row)])
-    labels = Variable(LongTensor(labels))
-    gen_imgs = generator(z, labels)
-    save_image(gen_imgs.data, "images/%d.png" % batches_done, nrow=n_row, normalize=True)
+# some hyper parameters
+ngpu = int(1)
+nz = int(110)
+ngf = int(64)
+ndf = int(64)
+num_classes = int(2)
+nc = 3
+
+netG = _netG(ngpu, nz)
+netG.apply(weights_init_normal)
+netD = _netD(ngpu, num_classes)
+netD.apply(weights_init_normal)
 
 
+# loss functions
+dis_criterion = nn.BCELoss()
+aux_criterion = nn.NLLLoss()
+
+
+inputs = torch.FloatTensor(32, 3, 128, 128)
+noise = torch.FloatTensor(32, nz, 1, 1)
+eval_noise = torch.FloatTensor(32, nz, 1, 1).normal_(0, 1)
+dis_label = torch.FloatTensor(32)
+aux_label = torch.LongTensor(32)
+real_label = 1
+fake_label = 0
+
+# if using cuda
+if cuda:
+    netD.cuda()
+    netG.cuda()
+    dis_criterion.cuda()
+    aux_criterion.cuda()
+    inputs, dis_label, aux_label = inputs.cuda(), dis_label.cuda(), aux_label.cuda()
+    noise, eval_noise = noise.cuda(), eval_noise.cuda()
+
+eval_noise_ = np.random.normal(0, 1, (32, nz))
+eval_label = np.random.randint(0, num_classes, 32)
+eval_onehot = np.zeros((32, num_classes))
+eval_onehot[np.arange(32), eval_label] = 1
+eval_noise_[np.arange(32), :num_classes] = eval_onehot[np.arange(32)]
+eval_noise_ = (torch.from_numpy(eval_noise_))
+eval_noise.data.copy_(eval_noise_.view(32, nz, 1, 1))
+
+
+# setup optimizer
+optimizerD = torch.optim.Adam(netD.parameters(), lr=0.0002, betas=(0.5, 0.999))
+optimizerG = torch.optim.Adam(netG.parameters(), lr=0.0002, betas=(0.5, 0.999))
+
+avg_loss_D = 0.0
+avg_loss_G = 0.0
+avg_loss_A = 0.0
 # ----------
 #  Training
 # ----------
 
-for epoch in range(200):
-    for i, (imgs, labels) in tqdm(enumerate(dl), total = len(dl.dataset)//32 + 1):
+for epoch in range(100):
+    for i, data in tqdm(enumerate(dl), total = len(dl.dataset)//32 + 1):
+        ############################
+        # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+        ###########################
+        # train with real
+        netD.zero_grad()
+        real_cpu, label = data
+        batch_size = real_cpu.size(0)
+        if cuda:
+            real_cpu = real_cpu.cuda()
 
-        batch_size = imgs.shape[0]
+        with torch.no_grad():
 
-        # Adversarial ground truths
-        valid = FloatTensor(batch_size, 1).fill_(1.0)
-        valid.requires_grad = False 
-    
-        fake = FloatTensor(batch_size, 1).fill_(0.0)
-        fake.requires_grad = False 
+            inputs.resize_as_(real_cpu).copy_(real_cpu)
+            dis_label.resize_(batch_size).fill_(real_label)
+            aux_label.resize_(batch_size).copy_(label)
+        dis_output, aux_output = netD(inputs)
 
-        # Configure input
-        real_imgs = imgs.type(FloatTensor)
-        labels = labels.type(LongTensor)
+        dis_errD_real = dis_criterion(dis_output, dis_label)
+        aux_errD_real = aux_criterion(aux_output, aux_label)
+        errD_real = dis_errD_real + aux_errD_real
+        errD_real.backward()
+        with torch.no_grad():
+            D_x = dis_output.mean()
 
-        # -----------------
-        #  Train Generator
-        # -----------------
+        # compute the current classification accuracy
+        accuracy = compute_acc(aux_output, aux_label)
 
-        optimizer_G.zero_grad()
+        # train with fake
+        with torch.no_grad():
+            noise.resize_(batch_size, nz, 1, 1).normal_(0, 1)
+        label = np.random.randint(0, num_classes, batch_size)
+        noise_ = np.random.normal(0, 1, (batch_size, nz))
+        class_onehot = np.zeros((batch_size, num_classes))
+        class_onehot[np.arange(batch_size), label] = 1
+        noise_[np.arange(batch_size), :num_classes] = class_onehot[np.arange(batch_size)]
+        noise_ = (torch.from_numpy(noise_))
+        with torch.no_grad():
+            noise.copy_(noise_.view(batch_size, nz, 1, 1))
+            aux_label.resize_(batch_size).copy_(torch.from_numpy(label))
 
-        # Sample noise and labels as generator input
-        z = FloatTensor(torch.randn((batch_size, 100)))
-        gen_labels = LongTensor(np.random.randint(0, 2, batch_size))
+        fake = netG(noise)
+        with torch.no_grad():
+            dis_label.fill_(fake_label)
+        dis_output, aux_output = netD(fake.detach())
+        dis_errD_fake = dis_criterion(dis_output, dis_label)
+        aux_errD_fake = aux_criterion(aux_output, aux_label)
+        errD_fake = dis_errD_fake + aux_errD_fake
+        errD_fake.backward()
+        with torch.no_grad():
+            D_G_z1 = dis_output.mean()
+        errD = errD_real + errD_fake
+        optimizerD.step()
 
-        # Generate a batch of images
-        gen_imgs = generator(z, gen_labels)
+        ############################
+        # (2) Update G network: maximize log(D(G(z)))
+        ###########################
+        netG.zero_grad()
+        with torch.no_grad():
+            dis_label.fill_(real_label)  # fake labels are real for generator cost
+        dis_output, aux_output = netD(fake)
+        dis_errG = dis_criterion(dis_output, dis_label)
+        aux_errG = aux_criterion(aux_output, aux_label)
+        errG = dis_errG + aux_errG
+        errG.backward()
+        with torch.no_grad():
+            D_G_z2 = dis_output.mean()
+        optimizerG.step()
 
-        # Loss measures generator's ability to fool the discriminator
-        validity, pred_label = discriminator(gen_imgs)
-        g_loss = 0.5 * (adversarial_loss(validity, valid) + auxiliary_loss(pred_label, gen_labels))
-
-        g_loss.backward()
-        optimizer_G.step()
-
-        # ---------------------
-        #  Train Discriminator
-        # ---------------------
-
-        optimizer_D.zero_grad()
-
-        # Loss for real images
-        real_pred, real_aux = discriminator(real_imgs)
-        d_real_loss = (adversarial_loss(real_pred, valid) + auxiliary_loss(real_aux, labels)) / 2
-
-        # Loss for fake images
-        fake_pred, fake_aux = discriminator(gen_imgs.detach())
-        d_fake_loss = (adversarial_loss(fake_pred, fake) + auxiliary_loss(fake_aux, gen_labels)) / 2
-
-        # Total discriminator loss
-        d_loss = (d_real_loss + d_fake_loss) / 2
-
-        # Calculate discriminator accuracy
-        pred = np.concatenate([real_aux.data.cpu().numpy(), fake_aux.data.cpu().numpy()], axis=0)
-        gt = np.concatenate([labels.data.cpu().numpy(), gen_labels.data.cpu().numpy()], axis=0)
-        d_acc = np.mean(np.argmax(pred, axis=1) == gt)
-
-        d_loss.backward()
-        optimizer_D.step()
+        # compute the average loss
+        curr_iter = epoch * len(dl) + i
+        all_loss_G = avg_loss_G * curr_iter
+        all_loss_D = avg_loss_D * curr_iter
+        all_loss_A = avg_loss_A * curr_iter
+        all_loss_G += errG.item()
+        all_loss_D += errD.item()
+        all_loss_A += accuracy
+        avg_loss_G = all_loss_G / (curr_iter + 1)
+        avg_loss_D = all_loss_D / (curr_iter + 1)
+        avg_loss_A = all_loss_A / (curr_iter + 1)
 
     if epoch % 10 == 0:
-        print(f"Epoch: {epoch}, Batch: {i}, D loss: {d_loss.item()}, G loss: {g_loss.item()}")
-        torch.save(generator.state_dict(), f"./gan/experiment_one/generator_ckpt_{epoch}.pt")
-        torch.save(discriminator.state_dict(), f"./gan/experiment_one/discriminator_ckpt_{epoch}.pt")
+        print(f"Epoch: {epoch}, Batch: {i}, D loss: {avg_loss_D}, G loss: {avg_loss_G }")
+        torch.save(netG.state_dict(), f"./acgan/experiment_one/generator_ckpt_{epoch}.pt")
+        torch.save(netD.state_dict(), f"./acgan/experiment_one/discriminator_ckpt_{epoch}.pt")
 
-        print(gen_imgs.shape)
+        print(fake.shape)
 
-        grid_img = torchvision.utils.make_grid(gen_imgs, nrow=11)
-        torchvision.utils.save_image(grid_img, f'./gan/experiment_one/generated_images_ckpt_{epoch}_cxr.png')
+        grid_img = torchvision.utils.make_grid(fake, nrow=11)
+        torchvision.utils.save_image(grid_img, f'./acgan/experiment_one/generated_images_ckpt_{epoch}_cxr.png')
 
-torch.save(generator.state_dict(), f"./gan/experiment_one/generator_ckpt_{epoch}.pt")
-torch.save(discriminator.state_dict(), f"./gan/experiment_one/discriminator_ckpt_{epoch}.pt")
+torch.save(netG.state_dict(), f"./acgan/experiment_one/generator_ckpt_{epoch}.pt")
+torch.save(netD.state_dict(), f"./acgan/experiment_one/discriminator_ckpt_{epoch}.pt")
+
