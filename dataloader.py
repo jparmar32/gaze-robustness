aba8e0ff150c12e6cd0d27a689fcdacb65657af6
jparmar32 <jsparmar@stanford.edu>
2022-02-24 18:31:41 -0800

    Synethic Creation and Dataset

--- dataloader.py
+++ dataloader.py
@@ -10,6 +10,7 @@ import pydicom
 import torchvision
 from skimage.transform import resize
 import math
+from skimage.filters import gaussian
 
 from utils import load_file_markers, get_data_transforms, load_gaze_attribute_labels, rle2mask, create_masked_image
 
@@ -234,6 +235,48 @@ class GanDataset(Dataset):
                 img = torch.cat([img, img, img])
 
         return img, int(self.labels[idx]), int(self.gaze_attr[idx])
+
+class SyntheticDataset(Dataset):
+    def __init__(self, split='train', blur=0, train_length=500, val_length=128, test_length=128):
+
+        self.split = split
+
+        if split == 'train':
+            self.length = train_length
+        elif split == "val":
+            self.length = val_length
+        else:
+            self.length = test_length
+
+        self.blur = blur 
+
+        ## load in labels
+        with open(f"./actdiff/data/synth_hard/{split}_labels.pkl", "rb") as f:
+            self.img_labels = pickle.load(f)
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, index):
+        img_path,label = self.img_labels[index]
+
+        img_path = "./actdiff" + img_path[1:]
+        seg_path = img_path.replace("img", "seg")
+
+        image = np.load(img_path)
+        seg = np.load(seg_path)
+
+        # If there is a segmentation, blur it a bit.
+        if (self.blur > 0) and (np.max(seg) != 0):
+            seg = gaussian(seg, self.blur, output=seg)
+            seg /= seg.max()
+
+        seg = (seg > 0) * 1.0
+
+        seg = torch.from_numpy(seg)
+        image = torch.from_numpy(image)
+
+        return image, int(label), seg
 
 
 def fetch_dataloaders(
